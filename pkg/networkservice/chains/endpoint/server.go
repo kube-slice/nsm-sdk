f281b2f8b389f31be34bdd2c0998b19e6421a32c
Md Suman Sarker <suman@appscode.com>
2025-09-30 11:28:13 +0600

    test: remove spire
    Signed-off-by: Md Suman Sarker <suman@appscode.com>

--- pkg/networkservice/chains/endpoint/server.go
+++ pkg/networkservice/chains/endpoint/server.go
@@ -21,6 +21,8 @@ package endpoint
 
 import (
 	"context"
+	authmonitor "github.com/networkservicemesh/sdk/pkg/tools/monitorconnection/authorize"
+	"github.com/networkservicemesh/sdk/pkg/tools/monitorconnection/next"
 	"net/url"
 
 	"google.golang.org/grpc"
@@ -34,16 +36,11 @@ import (
 	"github.com/google/uuid"
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
 
-	"github.com/networkservicemesh/sdk/pkg/networkservice/common/authorize"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/monitor"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/timeout"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/common/updatepath"
-	"github.com/networkservicemesh/sdk/pkg/networkservice/common/updatetoken"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/core/chain"
 	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
-	authmonitor "github.com/networkservicemesh/sdk/pkg/tools/monitorconnection/authorize"
-	"github.com/networkservicemesh/sdk/pkg/tools/monitorconnection/next"
-	"github.com/networkservicemesh/sdk/pkg/tools/token"
 )
 
 // Endpoint - aggregates the APIs:
@@ -106,10 +103,9 @@ func WithAdditionalFunctionality(additionalFunctionality ...networkservice.Netwo
 }
 
 // NewServer - returns a NetworkServiceMesh client as a chain of the standard Client pieces plus whatever
-func NewServer(ctx context.Context, tokenGenerator token.GeneratorFunc, options ...Option) Endpoint {
+func NewServer(ctx context.Context, options ...Option) Endpoint {
 	opts := &serverOptions{
 		name:                             "endpoint-" + uuid.New().String(),
-		authorizeServer:                  authorize.NewServer(authorize.Any()),
 		authorizeMonitorConnectionServer: authmonitor.NewMonitorConnectionServer(authmonitor.Any()),
 	}
 	for _, opt := range options {
@@ -122,8 +118,6 @@ func NewServer(ctx context.Context, tokenGenerator token.GeneratorFunc, options 
 		append([]networkservice.NetworkServiceServer{
 			updatepath.NewServer(opts.name),
 			begin.NewServer(),
-			updatetoken.NewServer(tokenGenerator),
-			opts.authorizeServer,
 			metadata.NewServer(),
 			timeout.NewServer(ctx),
 			monitor.NewServer(ctx, &mcsPtr),
